01836abf97f2038b0bbc41d43d98ada72ae12724
Julie Thrall <juliethrall10@gmail.com>
2019-06-25 12:45:21 -0700

    :sparkles: logo changes to random color on window scroll

--- assets/js/components/Navigation/navigation.js
+++ assets/js/components/Navigation/navigation.js
@@ -8,3 +8,23 @@
 // function toggle() {
 //     nav.classList.toggle('active')
 // }
+
+// const logo = document.querySelector("#logo-icon");
+// console.log(logo)
+
+window.addEventListener('scroll', randomColor);
+
+function randomColor() {
+    const logo = document.querySelector("#logo-icon");
+
+    let min = 100000;
+    let max = 500000;
+    let randomNumber = Math.floor(Math.random() * (+max - +min));
+    if (randomNumber < 10000) {
+        randomNumber += 10000;
+    }
+    logo.style.color = `#${randomNumber}`;
+
+
+
+}
